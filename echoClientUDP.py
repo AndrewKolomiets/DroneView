d09360f633479a98bf691b1156177cf00689aa34
Andrii Kolomiiets <andrew.kolomiets@gmail.com>
2025-03-07 08:37:32 +0200

    test scripts

--- echoClientUDP.py
+++ echoClientUDP.py
@@ -3,7 +3,8 @@ import time
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #server_address = '127.0.0.1'
-server_address = '192.168.137.141'
+#server_address = '192.168.137.141'
+server_address = '172.16.87.168'
 server_port = 31337
 client_socket.connect((server_address, server_port))
 
--- echoServerUDP.py
+++ echoServerUDP.py
@@ -3,15 +3,17 @@ import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#server_address = '0.0.0.0'
-server_address = '192.168.137.1'
-server_port = 31337
+server_address = '192.168.168.40'
+#server_address = '192.168.137.1'
+#server_address = '172.16.87.168'
+server_port = 20200
 
 server = (server_address, server_port)
 sock.bind(server)
 print("Listening on " + server_address + ":" + str(server_port))
 
 while True:
-	payload, client_address = sock.recvfrom(128)
-	print("Echoing data back to " + str(client_address))
-	sent = sock.sendto(payload, client_address)
+	payload, client_address = sock.recvfrom(4096)
+	print("from" + str(client_address) + " received \n" + str(payload) )
+	#print("Echoing data back to " + str(client_address))
+	#sent = sock.sendto(payload, client_address)
